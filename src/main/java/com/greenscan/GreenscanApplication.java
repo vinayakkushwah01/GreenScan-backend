52d0634d34d25afca791b1ce864436b34b19f08d
vinayak kushwah <kushwahvinayak1@gmail.com>
2025-09-20 15:12:45 +0530

    init Project

---
+++ src/main/java/com/greenscan/GreenscanApplication.java
@@ -0,0 +1,13 @@
+package com.greenscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenscanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenscan/GreenscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
