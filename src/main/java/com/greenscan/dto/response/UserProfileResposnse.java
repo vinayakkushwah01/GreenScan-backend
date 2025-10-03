6b5a26f4e1a42ac5d4dc29f8ac8947e1d761095e
vinayak kushwah <kushwahvinayak1@gmail.com>
2025-10-03 12:45:33 +0530

    add service method for end user and impl class with no method def

---
+++ src/main/java/com/greenscan/dto/response/UserProfileResposnse.java
@@ -0,0 +1,9 @@
+package com.greenscan.dto.response;
+
+public class UserProfileResposnse {
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String profilePictureUrl;
+
+}
